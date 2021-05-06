a5cca3e2bbcb51d8abf86d71becbec969a5d549c
melq <tu2sice@yahoo.co.jp>
2021-05-06 13:01:21 +0900

    :art: 見た目の完成

--- app/src/main/java/com/melq/mvvm/MainFragment.kt
+++ app/src/main/java/com/melq/mvvm/MainFragment.kt
@@ -1,17 +1,29 @@
 package com.melq.mvvm
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.melq.mvvm.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+    private lateinit var binding: FragmentMainBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val pref = activity?.getSharedPreferences("preference_root", Context.MODE_PRIVATE)
+        binding.count = pref?.getInt("count", 0)
     }
 }
